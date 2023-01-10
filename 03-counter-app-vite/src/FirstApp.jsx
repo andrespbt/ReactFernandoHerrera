9c70ed0bba88deb65c2254e98d1bd56143755a1a
Fernando Andres Poblete <andrespbt14@hotmail.com>
2023-01-10 11:07:49 -0300

    Seccion intro a React finalizada

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types';
-export const FirstApp = ({title, subTitle}) => {
-
+export const FirstApp = ({title, subTitle, name}) => {
+  if(!title){
+    throw new Error('El titulo no existe');
+  }
   return (
     <>
         <h1>{title}</h1>
-        <h2>My name is Andres</h2>
+        <h2>{subTitle}</h2>
+        <h2>{name}</h2>
     </>
   )
 }
 
 FirstApp.propTypes = {
-  title: PropTypes.string.isRequired
+  subTitle: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+}
 
+FirstApp.defaultProps = {
+  subTitle: 12345,
+  title: 'No hay titulo',
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 import { HelloWorldApp } from './HelloWorldApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={0} />
     </React.StrictMode>
 )
